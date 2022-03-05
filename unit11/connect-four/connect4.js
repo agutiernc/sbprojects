b6f62eef1abb3ebe450e8a3c291864fcfca5b609
agutiernc <agutiernc@gmail.com>
2022-03-04 23:18:47 -0800

    connect 4 project

--- unit11/connect-four/connect4.js
+++ unit11/connect-four/connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,33 +17,44 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let y = 0; y < HEIGHT; y++) {
+    board.push(Array(WIDTH).fill(null)) 
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.querySelector('#board')
 
   // TODO: add comment for this code
-  var top = document.createElement("tr");
+  // creates top row that player will select a column from
+  const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  // creates the cells that top row contains
+  for (let x = 0; x < WIDTH; x++) {
+    const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
+
   htmlBoard.append(top);
 
   // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  // creates the table with specified HEIGHT and WIDTH
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
+
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
     }
+
     htmlBoard.append(row);
   }
 }
@@ -52,36 +63,50 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  let result
+  
+  // check each cell with x coordinate. If not null, get result
+  for (let i = 0; i < HEIGHT; i++) {
+    board[i][x] ? i++ : result = i
+  }
+
+  return result
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  const piece = document.createElement('div')
+  piece.classList.add('piece', `p${currPlayer}`) // Add css style
+
+  const slot = document.getElementById(`${y}-${x}`);
+  slot.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg)
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
+  let y = findSpotForCol(x);
+
+  if (y === null) return;
 
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
+
+  board[y][x] = currPlayer
 
   // check for win
   if (checkForWin()) {
@@ -90,9 +115,13 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  const boardFilled = board.every(r => r.every(c => c !== null))
+  
+  if (boardFilled) return endGame(`It's a tie!`)
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer = 2 : currPlayer = 1
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -114,17 +143,19 @@ function checkForWin() {
   }
 
   // TODO: read and understand this code. Add comments to help you.
+  // checks each cell for a win whether it's vertical, horizontal, or diagonal
+  for (let y = 0; y < HEIGHT; y++) {
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+    for (let x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
+
     }
   }
 }
