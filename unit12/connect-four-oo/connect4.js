bb092c07af110a99af6d269238fb000f74a99946
agutiernc <agutiernc@gmail.com>
2022-03-06 19:54:28 -0800

    refactored code working

--- unit12/connect-four-oo/connect4.js
+++ unit12/connect-four-oo/connect4.js
@@ -78,8 +78,45 @@ class Game {
     this.board[y][x] = this.currPlayer
     this.placeInTable(y, x)
 
+    // check for win
+    if (this.checkForWin()) {
+      return this.endGame(`Player ${this.currPlayer} won!`)
+    }
+
+    // check for tie
+    if (this.board.every(row => row.every(cell => cell))) {
+      return endGame('Tie!');
+    }
+
     // switch players
     this.currPlayer = this.currPlayer === 1 ? 2 : 1
+  }
+
+  checkForWin() {
+    const _win = (cells) => cells.every(
+      ([y, x]) =>
+        y >= 0 &&
+        y < this.height &&
+        x >= 0 &&
+        x < this.width &&
+        this.board[y][x] === this.currPlayer
+    )
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        // get "check list" of 4 cells (starting here) for each of the different
+        // ways to win
+        const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+        const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+        const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+        const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  
+        // find winner (only checking each win-possibility as needed)
+        if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+          return true;
+        }
+      }
+    }
   }
 }
 
