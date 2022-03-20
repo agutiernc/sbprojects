207acf7bb811b865c09d9d7507dc3ad1b0663706
agutiernc <agutiernc@gmail.com>
2022-03-19 17:11:28 -0700

    added nav links & hid login on user login

--- unit16/hack-or-snooze-ajax-api/js/main.js
+++ unit16/hack-or-snooze-ajax-api/js/main.js
@@ -13,6 +13,7 @@ const $signupForm = $("#signup-form");
 const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
+const $navLinks = $('.main-nav-links')
 
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
--- unit16/hack-or-snooze-ajax-api/js/user.js
+++ unit16/hack-or-snooze-ajax-api/js/user.js
@@ -109,8 +109,7 @@ function saveUserCredentialsInLocalStorage() {
 
 function updateUIOnUserLogin() {
   console.debug("updateUIOnUserLogin");
-
+  hidePageComponents()
   $allStoriesList.show();
-
   updateNavOnLogin();
 }
