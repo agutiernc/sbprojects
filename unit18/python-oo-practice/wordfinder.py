6c13ef9fd22f6011f093d7e597809dbb44cdde98
agutiernc <agutiernc@gmail.com>
2022-04-02 19:50:06 -0700

    SpecialWordFinder Class done

--- unit18/python-oo-practice/wordfinder.py
+++ unit18/python-oo-practice/wordfinder.py
@@ -19,6 +19,8 @@ class WordFinder:
     '''
 
     def __init__(self, path):
+        '''Read file and return the number of words read'''
+
         word_file = open(path, 'r')
 
         self.words = self.parse(word_file)
@@ -26,7 +28,26 @@ class WordFinder:
         print(f'{len(self.words)} words read')
     
     def parse(self, word_file):
+        '''Parse the list of words file'''
+
         return [w.strip() for w in word_file]
 
     def random(self):
+        '''Return a random word'''
+
         return random.choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    '''Remove blank lines and commends from file
+    
+    >>> swf = SpecialWordFinder('file_name.txt')
+    3 words read
+
+    >>> swf.random in ['banana', 'strawberry', 'mango']
+    True
+    '''
+
+    def parse(self, word_file):
+        '''Parse word_file and exclude blank lines and comments'''
+
+        return [w.strip() for w in word_file if w.strip() and not w.startswith('#')]
