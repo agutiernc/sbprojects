2fce005bc32d4c70be350139631f1af478009b92
agutiernc <agutiernc@gmail.com>
2022-04-19 16:16:46 -0700

    boggle: step 5 - timer

--- unit19/flask-boggle/static/boggle.js
+++ unit19/flask-boggle/static/boggle.js
@@ -1,17 +1,19 @@
 let score = 0
+let startTimer = false
 
 async function handleSubmit(evt) {
   evt.preventDefault()
+
+  // enable timer
+  if (!startTimer) {
+    countDown()
+  }
 
   const $word = $('.word')
 
   let word = $word.val()
 
-  console.log(word)
-
   const res = await axios.get("/verify", { params: { word: word }});
-
-  console.log('res-data: ', res.data.result)
 
   if (res.data.result === 'not-word') {
     showMessage(word, 'is not a valid English word')
@@ -23,7 +25,7 @@ async function handleSubmit(evt) {
     scoreGame(word)
   }
 
-  $word.val('')
+  $word.val('').focus()
 }
 
 $('.verify-word').on('submit', handleSubmit)
@@ -32,7 +34,7 @@ $('.verify-word').on('submit', handleSubmit)
 const $msg = $('.msg').hide()
 
 function showMessage(word, message) {
-  return $msg.text(`${word} ${message}`).show().delay(3000).fadeOut('slow')
+  return $msg.text(`${word} ${message}`).show().delay(2000).fadeOut('slow')
 }
 
 function scoreGame(word) {
@@ -41,4 +43,23 @@ function scoreGame(word) {
   return $('.score').text(score)
 }
 
-// score amount is equal to length of a word
+function countDown() {
+  let seconds = 60
+
+  let interval_id = setInterval(() => {
+    if (seconds <= 0) {
+      clearInterval(interval_id)
+      startTimer = false
+
+      $('.timer').text('Times up!')
+      $('.verify-word :input').prop('disabled', true)
+    } else {
+      startTimer = true
+
+      $('.verify-word :input').prop('disabled', false)
+      $('.timer').text(seconds)
+    }
+
+    seconds -= 1
+  }, 1000)
+}
