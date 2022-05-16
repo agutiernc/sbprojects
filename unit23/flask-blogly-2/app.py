512c9441a700b19c2c76bb5e81e299e7a6289003
agutiernc <agutiernc@gmail.com>
2022-05-15 20:37:55 -0700

    added post details page and route

--- unit23/flask-blogly-2/app.py
+++ unit23/flask-blogly-2/app.py
@@ -136,3 +136,11 @@ def add_post(user_id):
     flash(f"Post '{new_post.title}' was added")
 
     return redirect(f'/users/{user_id}')
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    '''Display a post'''
+
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('/posts/show.html', post=post)
