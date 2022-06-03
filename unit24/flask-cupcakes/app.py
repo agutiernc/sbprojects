376c9e33e584bce5d3a657d406c11e654df3e0dd
agutiernc <agutiernc@gmail.com>
2022-06-03 16:48:20 -0700

    part 2 - get specific cupcake and also post new cupcake

--- unit24/flask-cupcakes/app.py
+++ unit24/flask-cupcakes/app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Cupcake
 import requests
@@ -21,7 +21,7 @@ connect_db(app)
 def main_page():
     '''Display main page'''
 
-    return render_template(index.html)
+    return render_template('index.html')
 
 @app.route('/api/cupcakes')
 def list_all_cupcakes():
@@ -30,3 +30,34 @@ def list_all_cupcakes():
     cupcakes = [c.to_dict() for c in Cupcake.query.all()]
 
     return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:cc_id>')
+def get_cupcake(cc_id):
+    '''Get data about a single cupcake'''
+
+    cc = Cupcake.query.get_or_404(cc_id)
+
+    return jsonify(cupcake=cc.to_dict())
+
+@app.route('/api/cupcakes', methods=['POST'])
+def add_cupcake():
+    '''Create a new cupcake.'''
+    
+    data = request.json
+
+    # create new cupcake
+    new_cupcake = Cupcake(
+        flavor=data['flavor'], 
+        size=data['size'], 
+        rating=data['rating'], 
+        image=data['image'] or None
+    )
+
+    # Add and commit new cupcake to DB
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    # convert to json
+    response_json = jsonify(cupcake=new_cupcake.to_dict())
+
+    return (response_json, 201)
