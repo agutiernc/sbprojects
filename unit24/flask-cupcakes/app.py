1c7072326a61ef6a6729db03dd13d21c2bb73c98
agutiernc <agutiernc@gmail.com>
2022-06-02 18:03:49 -0700

    part 2: getting all cupcakes route

--- unit24/flask-cupcakes/app.py
+++ unit24/flask-cupcakes/app.py
@@ -1,7 +1,8 @@
 """Flask app for Cupcakes"""
-from flask import Flask
+from flask import Flask, jsonify, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Cupcake
+import requests
 
 app = Flask(__name__)
 
@@ -15,3 +16,17 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route('/')
+def main_page():
+    '''Display main page'''
+
+    return render_template(index.html)
+
+@app.route('/api/cupcakes')
+def list_all_cupcakes():
+    '''Get data about all cupcakes.'''
+
+    cupcakes = [c.to_dict() for c in Cupcake.query.all()]
+
+    return jsonify(cupcakes=cupcakes)
