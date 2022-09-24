d17c1985b5296a9230923caad0e4ea57e6abb0e2
agutiernc <agutiernc@gmail.com>
2022-09-24 16:05:29 -0700

    lunchly reservations

--- unit35/express-lunchly/models/reservation.js
+++ unit35/express-lunchly/models/reservation.js
@@ -35,6 +35,27 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+  /** save reservation */
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+          VALUES ($1, $2, $3, $4)
+          RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      )
+
+      this.id = result.rows[0].id
+    } else {
+      await db.query(
+        `UPDATE reservations
+          SET start_at = $1, num_guests = $2, notes = $3
+          WHERE id=$4`,
+        [this.startAt, this.numGuests, this.notes, this.id]
+      )
+    }
+  }
 }
 
 
