d3cc61bb5d1575c1e6114e9dd888cab95edce952
agutiernc <agutiernc@gmail.com>
2022-10-04 18:21:13 -0700

    api validation ex. - part 1

---
+++ unit36/express-bookstore/routes/books.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = new express.Router();
+
+const jsonschema = require('jsonschema')
+const bookSchema = require('../schemas/bookSchema.json')
+
+const Book = require("../models/book");
+const ExpressError = require("../expressError");
+
+
+/** GET / => {books: [book, ...]}  */
+router.get("/", async function (req, res, next) {
+  try {
+    const books = await Book.findAll(req.query);
+
+    return res.json({ books });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** GET /[id]  => {book: book} */
+router.get("/:id", async function (req, res, next) {
+  try {
+    const book = await Book.findOne(req.params.id);
+
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** POST /   bookData => {book: newBook}  */
+router.post("/", async function (req, res, next) {
+  try {
+    const validBook = jsonschema.validate(req.body, bookSchema)
+
+    if (!validBook.valid) {
+      const listOfErrors = validBook.errors.map(e => e.stack)
+      const err = new ExpressError(listOfErrors, 400)
+
+      return next(err)
+    }
+
+    const book = await Book.create(req.body);
+
+    return res.status(201).json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** PUT /[isbn]   bookData => {book: updatedBook}  */
+router.put("/:isbn", async function (req, res, next) {
+  try {
+    const validBook = jsonschema.validate(req.body, bookSchema)
+
+    if (!validBook.valid) {
+      const listOfErrors = validBook.errors.map(e => e.stack)
+      const err = new ExpressError(listOfErrors, 400)
+
+      return next(err)
+    }
+
+    const book = await Book.update(req.params.isbn, req.body);
+
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** DELETE /[isbn]   => {message: "Book deleted"} */
+router.delete("/:isbn", async function (req, res, next) {
+  try {
+    await Book.remove(req.params.isbn);
+
+    return res.json({ message: "Book deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
---
+++ unit36/express-bookstore/server.js
@@ -0,0 +1,5 @@
+/** Server for bookstore. */
+
+const app = require("./app");
+
+app.listen(3000, () => console.log(`Server starting on port 3000`));
