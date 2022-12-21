b02a6e2afa9f28c3329bdf11a561e4db53d204e1
agutiernc <agutiernc@gmail.com>
2022-12-20 16:56:01 -0800

    modified homepage for user login/logout

--- unit43/react-jobly/frontend/src/homepage/Homepage.js
+++ unit43/react-jobly/frontend/src/homepage/Homepage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from '../users/UserContext'
+import './Homepage.css'
 
 const Homepage = () => {
+  const { currentUser } = useContext(UserContext)
+  
+  // shows message when user logs in
+  const showMsg = () => (
+    <div className='msg'>
+      <p>Welcome back, {currentUser.firstName}</p>
+    </div>
+  )
 
   return (
-    <div>
-      <h1>The Homepage</h1>
+    <div className='Homepage text-center'>
+      <h1 className='mt-5'>Jobly</h1>
+      <h3 className='mt-4'>For your Job search needs</h3>
+
+      {currentUser ? showMsg() : ''}
     </div>
   )
 }
