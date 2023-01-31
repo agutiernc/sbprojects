e305650527febd57e03d36d99db9f130bdf22582
agutiernc <agutiernc@gmail.com>
2023-01-30 19:25:07 -0800

    added dark mode to app

--- unit49/yodlr/frontend/src/App.js
+++ unit49/yodlr/frontend/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Container } from '@chakra-ui/react'
+import { Container, IconButton, useColorMode } from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 import ComponentRoutes from "./routes-nav/ComponentRoutes";
 
 
 const App = () => {
-  
+  const { colorMode, toggleColorMode } = useColorMode()
   
   return (
     <Router>
       <Container maxW='90%'>
+        <IconButton
+          icon={colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
+          isRound="true"
+          size="lg"
+          alignSelf="flex-end"
+          onClick={toggleColorMode}
+          my="5"
+        />
+
         <ComponentRoutes />
       </Container>
     </Router>
